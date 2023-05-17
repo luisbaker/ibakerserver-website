e80139957453a9b3744d6c617f04e847ab7f303f
Luis Baker <iluisferreira1516@gmail.com>
2023-05-17 22:55:46 +0200

    Refactored code structure:
    Modified index.php to improve performance and readability.
    Added new language files: lang/lang.en.php, lang/lang.fr.php, lang/lang.pt.php.
    Removed unused files: background.jpg, lang.en.php, lang.fr.php, lang.pt.php, style.css.
    Added new directories: css/ and img/ for future use.

--- lang.en.php
+++
@@ -1,9 +0,0 @@
-<?php
-$lang = array(
-    'PAGE_TITLE' => 'ASXXXXX - iBakerServer',
-    'PAGE_DESCRIPTION' => 'ASXXXXX belongs to iBakerServer association. We are a non-profit organization that shares knowledge through our Discord server and also provides game server hosting for those who cannot afford to pay for it.',
-    'MENU_PEERING' => 'Peering',
-    'MENU_ASSOCIATION' => 'Association',
-    'COPYRIGHT' => '© Luis - Baker'
-);
-?>
--- lang.pt.php
+++
@@ -1,9 +0,0 @@
-<?php
-$lang = array(
-    'PAGE_TITLE' => 'ASXXXXX - iBakerServer',
-    'PAGE_DESCRIPTION' => 'ASXXXXX pertence à associação iBakerServer. Somos uma organização sem fins lucrativos que compartilha conhecimento por meio de nosso servidor Discord e também fornece hospedagem de servidor de jogos para aqueles que não podem pagar por ela.',
-    'MENU_PEERING' => 'Interconexão',
-    'MENU_ASSOCIATION' => 'Associação',
-    'COPYRIGHT' => '© Luis - Baker'
-);
-?>
